fcd4b9862dc4965460c5e319bfcc0d868c0c5daa
jefrydco <jefrydco@gmail.com>
2019-04-24 06:17:01 +0700

    fix: redirect www to non-www

--- server/redirect-www-to-non-www.js
+++ server/redirect-www-to-non-www.js
@@ -1,3 +1,15 @@
-import secure from "ssl-express-www";
+import { isDev } from "../helpers";
 
-export default secure;
+// Taken from: https://stackoverflow.com/a/23816083/7711812
+export default (req, res, next) => {
+  const host = req.headers.host;
+  const url = req.url;
+  const canonicalDomain = "jefrydco.id";
+
+  if (!isDev && host !== canonicalDomain) {
+    res.writeHead(301, { Location: `https://${canonicalDomain}${url}` });
+    return res.end();
+  }
+
+  return next();
+};
