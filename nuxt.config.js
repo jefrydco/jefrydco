59ccc595f940632608a00917368ae1dcde576f9e
Jefry Dewangga <jefrydco@gmail.com>
2019-12-14 22:40:48 +0700

    refactor: pwa related settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -189,27 +189,6 @@ export default {
     ]
   },
 
-  meta: {
-    name: 'Jefrydco',
-    description: 'A personal site of Jefry Dewangga',
-    lang: 'id',
-    ogHost: 'https://jefrydco.id',
-    twitterCard: 'summary_large_image',
-    twitterSite: '@jefrydco',
-    twitterCreator: '@jefrydco'
-  },
-
-  // https://pwa.nuxtjs.org/modules/manifest.html
-  manifest: {
-    name: 'Jefrydco',
-    short_name: 'Jefrydco',
-    start_url: '/?utm_source=homescreen',
-    description: 'A personal site of Jefry Dewangga',
-    lang: 'id',
-    background_color: '#2D3748',
-    theme_color: '#2D3748'
-  },
-
   /*
    ** Customize the progress-bar color
    */
@@ -410,6 +389,45 @@ export default {
       )
       routes = flattenDeep(routes)
       return routes
+    }
+  },
+
+  pwa: {
+    // https://pwa.nuxtjs.org/modules/meta.html
+    meta: {
+      name: 'Jefrydco',
+      description: 'A personal site of Jefry Dewangga',
+      lang: 'id',
+      ogHost: 'https://jefrydco.id',
+      twitterCard: 'summary_large_image',
+      twitterSite: '@jefrydco',
+      twitterCreator: '@jefrydco'
+    },
+
+    // https://pwa.nuxtjs.org/modules/manifest.html
+    manifest: {
+      name: 'Jefrydco',
+      short_name: 'Jefrydco',
+      start_url: '/?utm_source=homescreen',
+      description: 'A personal site of Jefry Dewangga',
+      lang: 'id',
+      background_color: '#2D3748',
+      theme_color: '#2D3748'
+    },
+
+    // https://pwa.nuxtjs.org/modules/workbox.html
+    workbox: {
+      offlineAnalytics: true,
+      runtimeCaching: [
+        {
+          urlPattern: 'https://ajax.cloudflare.com/.*',
+          handler: 'staleWhileRevalidate'
+        },
+        {
+          urlPattern: 'https://d33wubrfki0l68.cloudfront.net/.*',
+          handler: 'staleWhileRevalidate'
+        }
+      ]
     }
   },
 
