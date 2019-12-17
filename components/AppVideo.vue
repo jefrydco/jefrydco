33462ec5fb8658993c085871b597fcd2737f8a1b
Jefry Dewangga <jefrydco@gmail.com>
2019-12-17 09:50:40 +0700

    feat: add video component

---
+++ components/AppVideo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :class="`embed embed__${size}`">
+    <video :src="videoRequired" autoplay loop muted playsinline />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: String,
+      default: '16/9',
+      validator: (val) => ['square', '16/9', '4/3', '21/9'].includes(val)
+    }
+  },
+  computed: {
+    videoRequired() {
+      return require(`~/assets/videos${this.src}`)
+    }
+  }
+}
+</script>
--- plugins/components.js
+++ plugins/components.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
 import AppImg from '~/components/AppImg'
+import AppVideo from '~/components/AppVideo'
 
 Vue.component('app-img', AppImg)
+Vue.component('app-video', AppVideo)
