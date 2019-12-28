110cf14dcd70583213434e61767a545322ebf5cb
Jefry Dewangga <jefrydco@gmail.com>
2019-12-28 18:43:44 +0700

    improvement(theme): improve logic
    if there is no cookie for theme, fallback to prefers color scheme

--- layouts/default.vue
+++ layouts/default.vue
@@ -25,19 +25,18 @@ export default {
   methods: {
     initColorScheme() {
       const isDark = Cookie.get('d')
-      if (isDark && Boolean(isDark)) {
-        this.isDark = true
-      } else {
-        this.isDark = false
-      }
-      if (window.matchMedia('(prefers-color-scheme)').media !== 'not all') {
+      if (isDark) {
+        if (parseInt(isDark)) {
+          this.isDark = true
+        }
+      } else if (
+        window.matchMedia('(prefers-color-scheme)').media !== 'not all'
+      ) {
         const darkModeMediaQuery = window.matchMedia(
           '(prefers-color-scheme: dark)'
         )
         if (darkModeMediaQuery.matches) {
           this.isDark = true
-        } else {
-          this.isDark = false
         }
         darkModeMediaQuery.addListener((e) => {
           const darkModeOn = e.matches
