5db3ee0a3cdf0e441036907562705e0e8782c87c
Jefry Dewangga <jefrydco@gmail.com>
2020-04-15 04:37:39 +0700

    feat: register vue recaptcha

--- plugins/components.client.js
+++ plugins/components.client.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import { VueScrollIndicator } from 'vue-scroll-indicator'
 import Intersect from 'vue-intersect'
+import VueRecaptcha from 'vue-recaptcha'
 
 Vue.component('vue-scroll-indicator', VueScrollIndicator)
 Vue.component('Intersect', Intersect)
+Vue.component(VueRecaptcha.name, VueRecaptcha)
