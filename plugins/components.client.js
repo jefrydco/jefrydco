aae5d1ca2c919ee574b42d0b31decb77167bfa87
Jefry Dewangga <jefrydco@gmail.com>
2020-04-14 10:13:24 +0700

    feat: register vue intersect as client component

--- plugins/components.client.js
+++ plugins/components.client.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
 import { VueScrollIndicator } from 'vue-scroll-indicator'
+import Intersect from 'vue-intersect'
 
 Vue.component('vue-scroll-indicator', VueScrollIndicator)
+Vue.component('Intersect', Intersect)
