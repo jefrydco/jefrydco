c41e911cd024fc070c177553ebf6c755d3903c46
Jefry Dewangga <jefrydco@gmail.com>
2021-01-18 11:46:45 +0700

    feat(app-amp-notice): add i18n

--- components/global/AppAmpNotice.vue
+++ components/global/AppAmpNotice.vue
@@ -1,8 +1,18 @@
+<i18n>
+{
+  "id": {
+    "amp_notice": "Kamu membuka versi AMP halaman ini, jika ingin menggunakan fitur interaktifnya silahkan beralih ke halaman versi biasanya melalui"
+  },
+  "en": {
+    "amp_notice": "You open the AMP version of this page, if you want to use the interactive features, please switch to the normal version via"
+  }
+}
+</i18n>
+
 <template>
   <blockquote v-if="$route.name.includes('amp')">
     <p>
-      You open the AMP version of this page, if you want to use the interactive
-      features, please switch to the normal version via,
+      {{ $t('amp_notice') }},
       <app-locale-path-link :to="to">
         {{ label }}
       </app-locale-path-link>
