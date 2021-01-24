eb409c4f06751c7ee79f23b0077ed8bd50a00200
Jefry Dewangga <jefrydco@gmail.com>
2021-01-24 23:03:04 +0700

    feat: display swap for google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -304,6 +304,7 @@ export default {
   // https://github.com/nuxt-community/google-fonts-module
   googleFonts: {
     download: true,
+    display: 'swap',
     families: {
       Bitter: true,
       'Merriweather Sans': true,
