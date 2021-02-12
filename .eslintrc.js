02d21b56d93ab60d8d0ad2fb9f4208df9de5cd29
Jefry Dewangga <jefrydco@gmail.com>
2021-02-12 11:54:46 +0700

    feat(eslint): disable no v html

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,8 @@ module.exports = {
     'no-param-reassign': 'off',
     'prettier/prettier': 'error',
     'vue/no-v-for-template-key': 'off',
-    'vue/no-v-for-template-key-on-child': 'off'
+    'vue/no-v-for-template-key-on-child': 'off',
+    'vue/no-v-html': 'off'
   },
   ignorePatterns: ['buildModules', 'libs']
 }
