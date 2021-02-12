63459597198c73a629d853b1e53ddce5a7120757
Jefry Dewangga <jefrydco@gmail.com>
2021-02-12 21:37:20 +0700

    fix(app-youtube-video): reduce width and height

--- components/global/AppYoutubeVideo.vue
+++ components/global/AppYoutubeVideo.vue
@@ -8,7 +8,7 @@
   >
     <transition name="fade">
       <div v-if="!isPlayed" class="youtube__image">
-        <img :src="thumbnailUrl" :alt="label" width="1920" height="1080" />
+        <img :src="thumbnailUrl" :alt="label" width="816" height="459" />
       </div>
       <div v-else class="youtube__embed">
         <iframe
