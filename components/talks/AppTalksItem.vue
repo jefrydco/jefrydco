8def107fd1d323b932c830529a0078581a545d0d
Jefry Dewangga <jefrydco@gmail.com>
2021-07-02 22:44:06 +0700

    feat(components): add talks

---
+++ components/talks/AppTalksItem.vue
@@ -0,0 +1,87 @@
+<template>
+  <app-card class="app-talks-item" :title="title" :slug="slug">
+    <template #image="">
+      <div class="app-talks-item__img">
+        <app-img :src="img" :alt="title" round="top" />
+      </div>
+    </template>
+    <template #default="">
+      <header class="app-talks-item__header">
+        <h2 class="app-talks-item__title">
+          {{ title }}
+        </h2>
+        <div class="app-talks-item__meta">
+          <time :datetime="startDate">
+            {{ formatDate(startDate) }}
+          </time>
+          •
+          <time :datetime="startDate">
+            {{ formatTime(startDate) }}
+          </time>
+          -
+          <time :datetime="endDate">
+            {{ formatTime(endDate) }}
+          </time>
+        </div>
+      </header>
+    </template>
+  </app-card>
+</template>
+
+<script lang="ts">
+import { formatDate } from '~/extendables'
+
+export default formatDate.extend({
+  props: {
+    img: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    slug: {
+      type: String,
+      required: true
+    },
+    startDate: {
+      type: String,
+      default: ''
+    },
+    endDate: {
+      type: String,
+      default: ''
+    }
+  }
+})
+</script>
+
+<style lang="postcss">
+/* purgecss start ignore */
+.app-talks-item {
+  background-color: var(--card-bg);
+
+  &__header {
+    @apply p-6 sm:px-10 sm:py-8;
+  }
+
+  &__title {
+    @apply capitalize font-bold text-2xl mt-0 mb-2;
+  }
+
+  &__img {
+    @apply h-64 rounded-t;
+    background-color: var(--inline-code-bg);
+  }
+
+  &__meta {
+    @apply leading-normal mb-4 text-sm;
+  }
+
+  img {
+    @apply h-64 w-full object-cover rounded-b-none;
+  }
+}
+/* purgecss end ignore */
+</style>
---
+++ components/talks/AppTalksSection.vue
@@ -0,0 +1,38 @@
+<template>
+  <details class="app-talks-section">
+    <summary class="app-talks-section__summary">
+      {{ title }}
+    </summary>
+    <slot />
+  </details>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
+
+<style lang="postcss">
+/* purgecss start ignore */
+.app-talks-section {
+  @apply max-w-4xl mx-auto;
+
+  &__summary {
+    @apply mb-5 mx-4 py-2 px-3 rounded-l-lg;
+    background-image: linear-gradient(
+      to right,
+      rgba(245, 158, 11, 0.2) 50%,
+      rgba(245, 101, 101, 0)
+    );
+  }
+}
+/* purgecss end ignore */
+</style>
